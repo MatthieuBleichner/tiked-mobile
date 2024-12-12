34392d8c43fdb2bdb710c1987384efa1d5e3776e
Matthieu Bleichner <matthieu258@hotmail.com>
2024-12-12 13:18:31 +0100

    use router.push rather than replace

--- app/citiesSelector/_layout.tsx
+++ app/citiesSelector/_layout.tsx
@@ -55,7 +55,7 @@ const LoginScreen = () => {
     if (pickerCity && pickerMarket) {
       setCurrentCity(pickerCity);
       setCurrentMarket(pickerMarket);
-      router.replace('/dashboard');
+      router.push('./dashboard');
     }
   };
 
